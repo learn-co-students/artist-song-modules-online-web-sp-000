e14514f7bd486b80851ff4027701b1196f415d62
Ariel <arielord@buffalo.edu>
2018-12-26 18:59:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,9 @@
 require 'pry'
+#require_relative '../config/environment.rb'
 
 class Song
+  extend Memorable
+  
   attr_accessor :name
   attr_reader :artist
 
@@ -16,14 +19,6 @@ class Song
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
