1a15245ddd963e5a7ec98beeb71d7ad47cc48014
elijahmm17 <elijahmm17@gmail.com>
2019-05-27 21:44:32 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
+require_relative '../lib/concerns/memorable'
+
 class Artist
+  extend Memorable
+  
   attr_accessor :name
   attr_reader :songs
 
@@ -14,28 +18,17 @@ class Artist
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
-
   def self.all
     @@artists
   end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
+  
   def add_song(song)
     @songs << song
     song.artist = self
   end
-
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
   def to_param
     name.downcase.gsub(' ', '-')
   end
