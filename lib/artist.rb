7dee8729621965fbae503dd27f6da177efce8cd7
Michael Lynch <michael@michaelrlynch.com>
2019-09-27 05:03:02 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,23 +15,11 @@ class Artist
     @@artists << self
     @songs = []
   end
-=begin
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-=end
+
   def self.all
     @@artists
   end
-=begin
-  def self.reset_all
-    self.all.clear
-  end
 
-  def self.count
-    self.all.count
-  end
-=end
   def add_song(song)
     @songs << song
     song.artist = self
@@ -39,9 +27,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -15,28 +15,12 @@ class Song
   def initialize
     @@songs << self
   end
-=begin
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-=end
+
   def self.all
     @@songs
   end
-=begin
-  def self.reset_all
-    self.all.clear
-  end
 
-  def self.count
-    self.all.count
-  end
-=end
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
