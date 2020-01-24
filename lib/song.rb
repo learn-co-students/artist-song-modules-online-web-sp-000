e629808015a1f2570f8ef4e9c2566fb6cbc203f0
sheriefCloud <sherief@farid.cloud>
2020-01-24 12:52:59 +0800

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,15 @@
 require 'pry'
 
+
+
+
 class Song
+  
+  
   attr_accessor :name
   attr_reader :artist
+  extend Memorable::ClassMethods
+
 
   @@songs = []
 
@@ -18,13 +25,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
