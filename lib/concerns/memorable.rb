2536aa5c610b3b4723c89d9d6408d3d1680403e2
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-18 16:35:19 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,5 @@
 module Memorable
-  
+  module ClassMethods
   def self.reset_all
       self.all.clear
   end
@@ -7,4 +7,5 @@ module Memorable
   def self.count
     self.all.count
   end
+end
 end
