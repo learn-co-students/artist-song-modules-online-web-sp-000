c63584b850a22c0adfb94873cd7967c5e8f74b52
C. Leow <chienleow1@gmail.com>
2020-04-08 21:12:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,15 +5,12 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    self.class.all << self
-  end
 
   def self.all
     @@songs
