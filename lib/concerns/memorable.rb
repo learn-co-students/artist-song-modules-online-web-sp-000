5da91e493c1524b056406f9f2309ab499e8eabbb
C. Leow <chienleow1@gmail.com>
2020-04-08 03:58:19 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,15 @@
 module Memorable
   
-  def self.reset_all
-    self.all.clear
+  module ClassMethods
+  
+    def self.reset_all
+      self.all.clear
+    end
+  
+    def self.count
+      self.all.count
+    end
+  
   end
 
-  def self.count
-    self.all.count
-  end
-  
 end
