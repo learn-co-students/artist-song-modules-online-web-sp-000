4c367daedc61f91e89beae3d5c95d73943403601
Robert Clarke <r.jason.clarke@gmail.com>
2020-12-02 01:27:35 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,13 @@
 require 'pry'
 
 class Artist
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include PARAMABLE
+
+
   attr_accessor :name
   attr_reader :songs
 
