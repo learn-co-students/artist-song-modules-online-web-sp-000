f2af6d80d9f117a4efa5f11d10339531542e4d05
Jason Shockness <jshock12@gmail.com>
2021-04-06 15:03:09 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,11 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   @@artists = []
 
   def initialize
