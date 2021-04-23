ee273b591015b4666a46d032c8fb322d0724c4c2
Drew Keat <thedrewkeat@gmail.com>
2021-04-23 10:24:37 -0600

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
-    
-    def reset_all
-        self.all.clear
+    module InstanceMethods
+        def initialize
+            self.class.all << self
+        end
     end
+    
+    module ClassMethods
+        def reset_all
+            self.all.clear
+        end
 
-    def count
-        self.all.count
+        def count
+            self.all.count
+        end
     end
-    
 end
