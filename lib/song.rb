88dd6d06d108c924fdd1cca7788c5b014a4f4543
Drew Keat <thedrewkeat@gmail.com>
2021-04-23 09:41:35 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,32 @@
+require_relative './concerns/memorable.rb'
 require 'pry'
 
 class Song
   attr_accessor :name
   attr_reader :artist
-
+  
   @@songs = []
-
+  
   def initialize
     @@songs << self
   end
-
+  
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
-
+  
   def self.all
     @@songs
   end
+  
+  extend Memorable
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
