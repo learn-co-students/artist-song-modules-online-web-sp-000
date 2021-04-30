2eaf992e0d304af860fef8f7a18e94db21eba16c
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-30 23:59:40 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,18 @@
 module Memorable
+  
+  module ClassMethods 
+    def reset_all
+      all.clear
+    end
 
-#  def self.reset_all
-#    self.all.clear
-#  end
-
-#  def self.count
-#    self.all.count
-#  end
-
+    def count
+      all.count
+    end
+  end 
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end 
 end 
