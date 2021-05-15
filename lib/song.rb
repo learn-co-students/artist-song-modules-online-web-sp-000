64993d2ebc35210fe03395c3e34e4e5c778e1beb
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-05-15 15:57:52 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,5 @@
 require 'pry'
 
-require_relative '../lib/concerns/memorable'
-require_relative '../lib/concerns/findable'
-require_relative '../lib/concerns/paramable'
-
 class Song
   attr_accessor :name
   attr_reader :artist
